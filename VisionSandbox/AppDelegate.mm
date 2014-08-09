f88b682e9058c66a7c6f9db776628ba53d37a207
Joel Brogan <joel.brogan@magnamirrors.com>
2014-08-08 21:56:15 -0400

    GL View now works with outline view user interface to navigate between different views.  Commence code cleaning!

--- VisionSandbox/AppDelegate.mm
+++ VisionSandbox/AppDelegate.mm
@@ -12,11 +12,14 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-	GLViewList*viewList = [[GLViewList alloc] initWithBackupPath:@"/Users/Shared/DIF Mapper/Settings/View List Settings/View List Backup.DIFmd"];
-	mainGLView.objectList = [[[GLObjectList alloc] initWithBackupPath:@"/Users/Shared/DIF Mapper/Settings/View List Settings/Main View List Backup.DIFmd"] autorelease];
+	GLViewList*viewList = [[GLViewList alloc] initWithBackupPath:@"/Users/joel/Documents/Programming/VisionSandbox/Settings/View List Backup.vis"];
+	mainGLView.objectList = [[[GLObjectList alloc] initWithBackupPath:@"/Users/joel/Documents/Programming/VisionSandbox/Settings/Main View List Backup.vis"] autorelease];
 	[viewList AddObject:mainGLView ForKeyPath:@"MainView"];
 	mainGLOutlineView.viewList = viewList;
-
+	[GLViewListCommand SetViewKeyPath:@"MainView" MaxImageSpaceRect:vector2Rect(0, 0, 400, 400)];
+	OpenImageHandler * h = [[OpenImageHandler alloc] initWithCVMat:cv::Mat(400,400,CV_8UC3,cv::Scalar(0,255,0)) Color:White BinaryImage:false];
+	[GLViewListCommand AddObject:h ToViewKeyPath:@"MainView" ForKeyPath:@"TestThing"];
+	[mainGLOutlineView reloadData];
 }
 
 @end
